0803cb590fb655831050cd3b437bc1687da6eb4c
Lovro Pleše <lplovropl@gmail.com>
2020-08-15 23:47:48 +0200

    Initial commit

---
+++ NFSPS_TestTrack_ModelFixer/NFSPS_TestTrack_ModelFixer.cpp
@@ -0,0 +1,328 @@
+// NFSPS_TestTrack_ModelFixer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+
+#define ESOLID_LIST_CHUNK1 0x80134000
+#define ESOLID_LIST_CHUNK2 0x80134010
+#define ESOLID_LIST_CHUNK3 0x80134100
+#define ESOLID_LIST_MAT_CHUNK 0x00134B02
+
+char *OutputFilename;
+char FileExt[16];
+
+struct stat st = { 0 };
+
+struct ProtoMatStruct
+{
+	int Pointer1;
+	int Pointer2;
+	int Pointer3;
+	int Unk1;
+	int Unk2;
+	int Unk3;
+	char unkbytes[8];
+	int Unk4;
+	int Unk5;
+	int Unk6;
+	short ProtoChange1;
+	short ProtoChange2;
+	int Unk7[6];
+	int Unk8;
+	char Unk9[4];
+	int Unk10[10];
+	int Unk11;
+	int Unk12;
+}ProtoMat;
+
+struct FinalMatStruct
+{
+	int Pointer1;
+	int Pointer2;
+	int Pointer3;
+	int Unk1;
+	int Unk2;
+	int Unk3;
+	char unkbytes[8];
+	int Unk4;
+	int Unk5;
+	int Unk6;
+	int ProtoChange1;
+	int ProtoChange2;
+	int Unk7[6];
+	int Unk8;
+	char Unk9[4];
+	int Unk10[10];
+	int Unk11;
+}FinalMat, PreDefMat;
+
+void *OutFile;
+
+void PreDefMatWrite()
+{
+	PreDefMat.Pointer1 = 0;
+	PreDefMat.Pointer2 = 4;
+	PreDefMat.Pointer3 = 0;
+	PreDefMat.Unk1 = 1;
+	PreDefMat.Unk2 = 2;
+	PreDefMat.Unk3 = 3;
+	PreDefMat.unkbytes[0] = 0xFF;
+	PreDefMat.unkbytes[1] = 0xFF;
+	PreDefMat.unkbytes[2] = 0xFF;
+	PreDefMat.unkbytes[3] = 0xFF;
+	PreDefMat.unkbytes[4] = 0x0;
+	PreDefMat.unkbytes[5] = 0x1;
+	PreDefMat.unkbytes[6] = 0x2;
+	PreDefMat.unkbytes[7] = 0x3;
+	PreDefMat.Unk4 = 0x4FF;
+	PreDefMat.Unk5 = 0xA10802BF;
+	PreDefMat.Unk6 = 0x2A4000;
+	PreDefMat.ProtoChange1 = 0xB4;
+	PreDefMat.ProtoChange2 = 0xFF;
+	PreDefMat.Unk8 = 0x21BC;
+	PreDefMat.Unk9[0] = 0;
+	PreDefMat.Unk9[1] = 0;
+	PreDefMat.Unk9[2] = 0x44;
+	PreDefMat.Unk9[3] = 0x80;
+}
+
+void ProtoToFinalBridge()
+{
+	FinalMat.Pointer1 = ProtoMat.Pointer1;
+	FinalMat.Pointer2 = ProtoMat.Pointer2;
+	FinalMat.Pointer3 = ProtoMat.Pointer3;
+	FinalMat.Unk1 = ProtoMat.Unk1;
+	FinalMat.Unk2 = ProtoMat.Unk2;
+	FinalMat.Unk3 = ProtoMat.Unk3;
+	memcpy(&FinalMat.unkbytes, &ProtoMat.unkbytes, sizeof(ProtoMat.unkbytes));
+	FinalMat.Unk4 = ProtoMat.Unk4;
+	FinalMat.Unk5 = ProtoMat.Unk5;
+	FinalMat.Unk6 = ProtoMat.Unk6;
+	FinalMat.ProtoChange1 = ProtoMat.ProtoChange1;
+	FinalMat.ProtoChange2 = ProtoMat.ProtoChange2;
+	memcpy(&FinalMat.Unk7, &ProtoMat.Unk7, sizeof(ProtoMat.Unk7));
+	FinalMat.Unk8 = ProtoMat.Unk8;
+	memcpy(&FinalMat.Unk9, &ProtoMat.Unk9, sizeof(ProtoMat.Unk9));
+	memcpy(&FinalMat.Unk10, &ProtoMat.Unk10, sizeof(ProtoMat.Unk10));
+	FinalMat.Unk11 = ProtoMat.Unk11;
+}
+
+int ParseAndFixMaterial(FILE *f, int RelativeEnd, int ChunkSize)
+{
+	bool bFinishedPadding = false;
+	int MatsOffset = 0;
+	int PaddingStart = 0;
+	int PaddingSize = 0;
+	int TrueChunkSize = 0;
+	int NumMaterial = 0;
+	//printf("Fixing material here!!\n");
+	PaddingStart = ftell(f);
+	while (ftell(f) < RelativeEnd)
+	{
+
+		if (!bFinishedPadding)
+		{
+			if (fgetc(f) != 0x11)
+			{
+				bFinishedPadding = true;
+				fseek(f, -1, SEEK_CUR);
+				MatsOffset = ftell(f);
+				PaddingSize = PaddingStart - MatsOffset;
+				TrueChunkSize = ChunkSize - PaddingSize;
+				NumMaterial = TrueChunkSize / sizeof(ProtoMatStruct);
+				break;
+			}
+		}
+		else
+		{
+			fseek(f, 4, SEEK_CUR);
+		}
+	}
+	//fread(&ProtoMat, sizeof(ProtoMatStruct), 1, f);
+	//fseek(f, RelativeEnd, SEEK_CUR);
+	//fread(&ProtoMat, sizeof(ProtoMatStruct), 1, f);
+
+	//printf("NumMaterial = %d\n", NumMaterial);
+
+	for (unsigned int i = 0; i < NumMaterial; i++)
+	{
+		fread(&ProtoMat, sizeof(ProtoMatStruct), 1, f);
+		ProtoToFinalBridge();
+		printf("Patching output memory image at @ 0x%X !\n", MatsOffset);
+		// patch memory image of the file now
+		memcpy((void*)((int)OutFile + MatsOffset), &FinalMat, sizeof(FinalMatStruct));
+		MatsOffset += sizeof(ProtoMatStruct);
+		fseek(f, MatsOffset, SEEK_SET);
+	}
+
+	//ProtoToFinalBridge();
+	//printf("Patching output memory image at @ 0x%X !\n", MatsOffset);
+	//// patch memory image of the file now
+	//memcpy((void*)((int)OutFile + MatsOffset), &FinalMat, sizeof(FinalMatStruct));
+
+	fseek(f, PaddingStart, SEEK_SET);
+	fseek(f, ChunkSize, SEEK_CUR);
+
+	//memcpy((void*)((int)OutFile + MatsOffset), &PreDefMat, sizeof(FinalMatStruct));
+
+	return 0;
+}
+
+int SearchChunk4(FILE *f, int RelativeEnd)
+{
+	int ChunkType;
+	int ChunkSize;
+
+	while (ftell(f) < RelativeEnd)
+	{
+		fread(&ChunkType, sizeof(int), 1, f);
+		fread(&ChunkSize, sizeof(int), 1, f);
+
+		if (ChunkType == ESOLID_LIST_MAT_CHUNK)
+		{
+			printf("Found esolid mat chunk 0x%X size 0x%X @ 0x%X\n", ChunkType, ChunkSize, ftell(f) - sizeof(int) * 2);
+			ParseAndFixMaterial(f, ftell(f) + ChunkSize, ChunkSize);
+		}
+		else
+			fseek(f, ChunkSize, SEEK_CUR);
+	}
+	return 0;
+}
+
+int SearchChunk3(FILE *f, int RelativeEnd)
+{
+	int ChunkType;
+	int ChunkSize;
+	
+	while (ftell(f) < RelativeEnd)
+	{
+		fread(&ChunkType, sizeof(int), 1, f);
+		fread(&ChunkSize, sizeof(int), 1, f);
+
+		if (ChunkType == ESOLID_LIST_CHUNK3)
+		{
+			printf("Found esolid chunk 3 0x%X size 0x%X @ 0x%X\n", ChunkType, ChunkSize, ftell(f) - sizeof(int) * 2);
+			SearchChunk4(f, ftell(f) + ChunkSize);
+		}
+		else
+			fseek(f, ChunkSize, SEEK_CUR);
+	}
+	return 0;
+}
+
+int SearchChunk2(FILE *f, int RelativeEnd)
+{
+	int ChunkType;
+	int ChunkSize;
+
+	while (ftell(f) < RelativeEnd)
+	{
+		fread(&ChunkType, sizeof(int), 1, f);
+		fread(&ChunkSize, sizeof(int), 1, f);
+
+		if (ChunkType == ESOLID_LIST_CHUNK2)
+		{
+			printf("Found esolid chunk 2 0x%X size 0x%X @ 0x%X\n", ChunkType, ChunkSize, ftell(f) - sizeof(int) * 2);
+
+			//printf("Patching output memory image at @ 0x%X !\n", ftell(f) - sizeof(int) * 2);
+			//*(int*)((int)OutFile + ftell(f) - sizeof(int) * 2) = 0;
+			//fseek(f, ChunkSize, SEEK_CUR);
+
+			SearchChunk3(f, ftell(f) + ChunkSize);
+		}
+		else
+			fseek(f, ChunkSize, SEEK_CUR);
+	}
+	return 0;
+}
+
+
+int FixMaterialChunks(const char* InFilename, const char* OutFilename)
+{
+	if (stat(InFilename, &st) == -1)
+	{
+
+		printf("ERROR: Can't open file %s for reading!\n", InFilename);
+		perror("ERROR");
+		return -1;
+	}
+
+	FILE *f = fopen(InFilename, "rb");
+	
+	int ChunkType;
+	int ChunkSize;
+	OutFile = malloc(st.st_size); // loading the entire file into memory, idc atm.
+	if (!f)
+	{
+		printf("ERROR: Can't open file %s for reading!\n", InFilename);
+		perror("ERROR");
+		return -1;
+	}
+
+	FILE *fout = fopen(OutFilename, "wb");
+	if (!fout)
+	{
+		printf("ERROR: Can't open file %s for writing!\n", OutFilename);
+		perror("ERROR");
+		return -1;
+	}
+
+	printf("Loading file %s into memory (size: %d bytes / 0x%X) ...\n", InFilename, st.st_size, st.st_size);
+	fread(OutFile, st.st_size, 1, f);
+	fseek(f, 0, SEEK_SET);
+
+	while (ftell(f) < st.st_size)
+	{
+
+		fread(&ChunkType, sizeof(int), 1, f);
+		fread(&ChunkSize, sizeof(int), 1, f);
+
+		if (ChunkType == ESOLID_LIST_CHUNK1)
+		{
+			printf("Found esolid chunk 1 0x%X size 0x%X @ 0x%X\n", ChunkType, ChunkSize, ftell(f) - sizeof(int) * 2);
+			SearchChunk2(f, ftell(f) + ChunkSize);
+		}
+		else
+		{
+			printf("Skipping chunk 0x%X size 0x%X @ 0x%X\n", ChunkType, ChunkSize, ftell(f) - sizeof(int) * 2);
+			fseek(f, ChunkSize, SEEK_CUR);
+		}
+
+	}
+	// dump the modded memory image...
+	printf("Writing %s (size: %d bytes / 0x%X) to disk!\n", OutFilename, st.st_size, st.st_size);
+	fwrite(OutFile, st.st_size, 1, fout);
+
+	free(OutFile);
+	fclose(fout);
+	fclose(f);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("NFS ProStreet Proto Material to Final patcher\n\n");
+	if (argc < 2)
+	{
+		printf("ERROR: Too few arguments.\nUSAGE: %s ESOLIDBUNDLE [OUTFILENAME]\n", argv[0]);
+		return -1;
+	}
+	if (argc == 2)
+	{
+		char* PatchPoint;
+		OutputFilename = (char*)calloc(strlen(argv[1]), sizeof(char) + 8);
+		strcpy(OutputFilename, argv[1]);
+		PatchPoint = strrchr(OutputFilename, '.');
+		strcpy(FileExt, PatchPoint);
+		sprintf(PatchPoint, "_patched%s", FileExt);
+	}
+	else
+	{
+		OutputFilename = argv[2];
+	}
+	FixMaterialChunks(argv[1], OutputFilename);
+    return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# NFS Pro Street - Test Track fixer
+This is a simple patching utility to attempt converting the material chunks in the prototype/testing models to the final format.
+
+It is not fully finished as it still has broken materials visible in the game.
+
+Use this at your own risk.
+
+# Usage
+## Windows GUI way:
+- Drag and drop a track stream you need to convert (e.g. STREAML6R_TestTrack.BUN) to the executable
+- You should have a file with _patched at the end (e.g. STREAML6R_TestTrack_patched.BUN)
+
+## CMD / Terminal way:
+- NFSPS_TestTrack_ModelFixer StreamBundle [OutFileName](optional)
+
+Afterwards:
+
+- Backup your old file, and place your patched file in the TRACKS folder with the correct name (e.g. STREAML6R_TestTrack_patched.BUN back to STREAML6R_TestTrack.BUN)
+
+# Credits
+- heyitsleo / LeoCodes21 - figuring out the differences in the formats
